bdb04b035606b23c8dc380abe373feb777011cba
jlawthom <jacklawthom@yahoo.co.uk>
2023-06-22 20:21:34 +0100

    Init Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express();
+
+app.listen(5001, () => console.log('Listening on port 5001'));
+
+app.get('/', (req, res) => res.json('API Running!! You better go catch it'))
