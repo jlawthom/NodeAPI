221a55e81ebe85a21727b22f7eeb270ef78337d7
jlawthom <jacklawthom@yahoo.co.uk>
2023-06-22 20:38:48 +0100

    v2

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.listen(5001, () => console.log('Listening on port 5001'));
 
-app.get('/', (req, res) => res.json('API Running!! You better go catch it'))
+app.get('/', (req, res) => res.json('API Running!! You better go catch it -v2'))
